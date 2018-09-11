be20389847258af978e0bf296fde432356d781fd
Programming Club VNIT <40888766+proclubvnit@users.noreply.github.com>
2018-09-11 16:51:06 +0530

    Create solution.c

---
+++ Code_War/Yash needs your help/solution.c
@@ -0,0 +1,40 @@
+//EDITORIAL
+#include <stdio.h>
+#define MAXN 10000
+int main(){
+
+	int t;
+	scanf("%d",&t);
+	while(t--){
+
+		int n,val,maxi;
+		scanf("%d",&n);
+		int freq[2*MAXN+1]={0};
+
+		//1st input
+		scanf("%d",&val);
+		maxi=val;
+		freq[val+MAXN]++;
+		printf("%d\n",maxi);
+		//n-1 inputs
+		for(int i=1;i<n;i++){
+
+		  scanf("%d",&val);
+			freq[val+MAXN]++;
+			if(freq[val+MAXN] > freq[maxi+MAXN])
+			{
+				maxi = val;
+			}
+			else if(freq[val+MAXN] == freq[maxi+MAXN])
+			{
+				if(val < maxi)
+					maxi = val;
+			}
+
+		printf("%d\n",maxi);
+		}
+
+
+	}
+	return 0;
+}
